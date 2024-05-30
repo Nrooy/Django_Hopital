beefd61ba9c9502e6c51f2931f6393cce272149f
Nrooy <duyhuyy1999@gmail.com>
2024-05-30 14:41:42 +0700

    huy

--- Patient/patient_service/models.py
+++ Patient/patient_service/models.py
@@ -25,7 +25,11 @@ class Patient(models.Model):
     address = models.CharField(max_length=255, blank=True, null=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     email = models.EmailField(max_length=100, blank=True, null=True)
-    health_insurance = models.OneToOneField(HealthInsurance, on_delete=models.SET_NULL, null=True, blank=True)
+    health_insurance = models.EmbeddedModelField(
+        model_container=HealthInsurance,
+        blank=True,
+        null=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
