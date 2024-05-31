bce040512a7c5fe7ebc9ecce5c707f9cf3b5009e
thanh.nt <thanh.nt@tinasoft.com.vn>
2024-06-01 00:53:17 +0700

    sua 1 ty

--- appointment_service/appointment/views.py
+++ appointment_service/appointment/views.py
@@ -1,17 +1,16 @@
 from rest_framework.permissions import AllowAny
-from rest_framework import viewsets, status, permissions
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 from rest_framework.pagination import PageNumberPagination
 from .models import Appointment
 from .serializers import AppointmentSerializer
-import requests
+from .filters import AppointmentFilter
 
 class ListCreateAppointmentAPIView(ListCreateAPIView):
     serializer_class = AppointmentSerializer
     queryset = Appointment.objects.all()
     permission_classes = [AllowAny]
+    filterset_class = AppointmentFilter
 
 class RetrieveUpdateDestroyAppointmentAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = AppointmentSerializer
